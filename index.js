635926b3d77c7a31b68c27abb95f8425ddc58ab6
rockbot <raquel@rckbt.me>
2014-05-16 15:16:01 -0700

    moves config bits to a separate file

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
-var Hapi = require('hapi')
-  , path = require('path')
+var Hapi = require('hapi'),
+    config = require('./config.js')
 
-var options = {
-  views: {
-    engines: {
-      hbs: 'handlebars'
-    },
-    // path: path.resolve(__dirname, "templates"),
-    partialsPath: path.resolve(__dirname, 'hbs-partials'),
-    helpersPath: path.resolve(__dirname, 'hbs-helpers')
-  }
-};
-
-var dbOptions = {
-  "couchAuth": "admin:admin",
-  "registryCouch": "http://localhost:15984/"
-}
-
-var server = new Hapi.Server('localhost', 15443, options)
+var server = new Hapi.Server(config.host, config.port, config.server)
 
 server.route({
   path: '/static/{path*}',
@@ -31,9 +15,7 @@ server.route({
   }
 });
 
-
-
-server.pack.require({'./facets/company': null, './facets/registry': null, './servers/hapi-couchdb': dbOptions}, function(err) {
+server.pack.require({'./facets/company': null, './facets/registry': null, './servers/hapi-couchdb': config.couch}, function(err) {
     if (err) throw err;
     server.start(function() {
         console.log('Hapi server started @ ' + server.info.uri);
